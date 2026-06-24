fdda5383ea034c455809812a49e1773da8912ff7
Richard Sabbarton <richard.sabbarton@gmail.com>
2026-06-24 16:05:14 +0100

    Add new icons, profile pictures, and sitemap configuration
    - Added new PNG icons for start animation, stop animation, straight line tool, and up arrow.
    - Included default and user-specific profile pictures.
    - Created a spritesheet for the scaredy ball.
    - Added a robots.txt file to manage web crawler access.
    - Generated a static sitemap XML for improved SEO.
    - Configured Vite build settings for output directory and sourcemaps.

---
+++ src/vite.config.ts
@@ -0,0 +1,12 @@
+/** @type {import('vite').UserConfig} */
+export default {
+  // config options
+  build: {
+    outDir: "../dist",
+    emptyOutDir: true,
+    minify: "false",
+    //allowJs: true,
+    //target: "esnext",
+    sourcemap: true,
+  },
+};
